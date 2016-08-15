6823d21b7ebfa111fbc2a4d5a4904c538b988c43
Jonathon Chappell <jc300329@ad.jcu.edu.au>
2016-08-15 13:21:23 +1000

    Fixed debugging errors

--- DebuggingExercises/DebugThree1.java
+++ DebuggingExercises/DebugThree1.java
@@ -3,16 +3,16 @@ public class DebugThree1
 {
    public static void main(String args[])
    {
-      double myCheck = 50.00;
+      double mycheck = 50.00;
       double yourCheck = 19.95;
       System.out.println("Tips are");
       calcTip(mycheck);
-      calctip(yourCheck);
+      calcTip(yourCheck);
     }
-    public static int calcTip(int bill)
+    public static void calcTip(double bill)
     {
       final double RATE = 0.15;
-      tip = bill + RATE;
+      double tip = bill + RATE;
       System.out.println("The tip should be at least " + tip);
     }
 }
